03eb3af338f8e52ed46bb839f7a4d60db1da70b4
Vigneshwaran <Vigneshwaran.Somasundaram@elders.com.au>
2020-06-29 16:16:07 +0530

    Updates

---
+++ precache-manifest.8fadf6f8b8fb34da2f5b0d874b3e8ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c141e7eae5721379ec12149e0485988",
+    "url": "/FE-HAWeb/index.html"
+  },
+  {
+    "revision": "86b5de07504e02018b31",
+    "url": "/FE-HAWeb/static/css/2.7ef9b196.chunk.css"
+  },
+  {
+    "revision": "e8a99cba27572be01c97",
+    "url": "/FE-HAWeb/static/css/main.3d3eab03.chunk.css"
+  },
+  {
+    "revision": "86b5de07504e02018b31",
+    "url": "/FE-HAWeb/static/js/2.296bc734.chunk.js"
+  },
+  {
+    "revision": "9bd19cc7db757a2d1f88c0265a862edb",
+    "url": "/FE-HAWeb/static/js/2.296bc734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a99cba27572be01c97",
+    "url": "/FE-HAWeb/static/js/main.0794f5ec.chunk.js"
+  },
+  {
+    "revision": "e089e2359cf48a1c1505",
+    "url": "/FE-HAWeb/static/js/runtime-main.c5f09b49.js"
+  }
+]);
